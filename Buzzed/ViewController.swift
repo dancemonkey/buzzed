413949d80f6dc21f6e7bb58787c36b49d28aef9b
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2017-02-18 20:20:08 -0500

    Initial Commit

---
+++ Buzzed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buzzed
+//
+//  Created by Drew Lanning on 2/18/17.
+//  Copyright © 2017 Drew Lanning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
