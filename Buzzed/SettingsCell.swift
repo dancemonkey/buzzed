f40b9c401200440357b5f6faa52efa9d58805e0e
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2017-03-02 20:37:47 -0500

    styling settings screen

---
+++ Buzzed/SettingsCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsCell.swift
+//  Buzzed
+//
+//  Created by Drew Lanning on 3/2/17.
+//  Copyright © 2017 Drew Lanning. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
