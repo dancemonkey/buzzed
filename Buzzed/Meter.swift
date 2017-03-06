521d70ce690495054da43690559958e918859d19
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2017-03-05 20:00:24 -0500

    got needle rotate functionality working (tossed out drawing, just have a UIimage that rotates)

--- Buzzed/Meter.swift
+++ Buzzed/Meter.swift
@@ -12,6 +12,7 @@ class Meter: UIView {
   
   @IBOutlet weak var background: UIImageView!
   @IBOutlet weak var view: UIView!
+  @IBOutlet weak var needleImg: UIImageView!
   
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
@@ -20,6 +21,8 @@ class Meter: UIView {
     self.addSubview(view)
     view.frame = self.bounds
     
+    // TODO: this should eventually call core data for current daily caffeine level, not minLevel
+    // INFO: minLevel is basically zero on the meter
     setLevel(to: minLevel)
     
     let dm = DataManager()
@@ -31,11 +34,15 @@ class Meter: UIView {
     rotateNeedle()
   }
   
+  private func setZeroLevel(to level: Double) {
+    
+  }
+  
   private var _level: Double = 0
   var level: Double {
     return _level
   }
-  private let _minLevel: Double = 0
+  private let _minLevel: Double = -75.0
   var minLevel: Double {
     return _minLevel
   }
@@ -50,41 +57,41 @@ class Meter: UIView {
   
   private func rotateNeedle() {
     UIView.animate(withDuration: 0.5) { 
-//      self.needle.transform = CGAffineTransform(rotationAngle: CGFloat(self._level) * CGFloat.pi / 180)
+      self.needleImg.transform = CGAffineTransform(rotationAngle: CGFloat(self._level) * CGFloat.pi / 180)
     }
   }
   
-  private func drawLine() {
-    let start = CGPoint(x: background.frame.width/2, y: background.frame.height)
-    let end = CGPoint(x: background.frame.width/2, y: 0)
-    background.image = drawLineOnImage(size: background.frame.size, image: background.image!, from: start, to: end)
-  }
-  
-  override func draw(_ rect: CGRect) {
-    drawLine()
-  }
-  
-  private func drawLineOnImage(size: CGSize, image: UIImage, from: CGPoint, to: CGPoint) -> UIImage {
-    
-    UIGraphicsBeginImageContext(size)
-    image.draw(at: CGPoint.zero)
-    
-    let context = UIGraphicsGetCurrentContext()
-    
-    context!.setLineWidth(2.0)
-    context!.setStrokeColor(UIColor.white.cgColor)
-    
-    context!.move(to: from)
-    context!.addLine(to: to)
-    
-    context!.strokePath()
-    
-    let resultImage = UIGraphicsGetImageFromCurrentImageContext()
-    
-    UIGraphicsEndImageContext()
-    
-    return resultImage!
-  }
+//  private func drawNeedle() {
+//    let start = CGPoint(x: background.frame.width/2, y: background.frame.height)
+//    let end = CGPoint(x: background.frame.width/2, y: 0)
+//    background.image = drawLineOnImage(size: background.frame.size, image: needleImg.image!, from: start, to: end)
+//  }
+//  
+//  override func draw(_ rect: CGRect) {
+//    
+//  }
+//  
+//  private func drawLineOnImage(size: CGSize, image: UIImage, from: CGPoint, to: CGPoint) -> UIImage {
+//    
+//    UIGraphicsBeginImageContext(size)
+//    image.draw(at: CGPoint.zero)
+//    
+//    let context = UIGraphicsGetCurrentContext()
+//    
+//    context!.setLineWidth(2.0)
+//    context!.setStrokeColor(UIColor.white.cgColor)
+//    
+//    context!.move(to: from)
+//    context!.addLine(to: to)
+//    
+//    context!.strokePath()
+//    
+//    let resultImage = UIGraphicsGetImageFromCurrentImageContext()
+//    
+//    UIGraphicsEndImageContext()
+//    
+//    return resultImage!
+//  }
 
   
 }
