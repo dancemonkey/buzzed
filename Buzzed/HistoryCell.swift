169dbbee656e52e919ea9f766bc61e7afda4df51
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2017-03-13 09:25:36 -0400

    starting work on historyVC and cells

---
+++ Buzzed/HistoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryCell.swift
+//  Buzzed
+//
+//  Created by Drew Lanning on 3/13/17.
+//  Copyright © 2017 Drew Lanning. All rights reserved.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
---
+++ Buzzed/HistoryVC.swift
@@ -0,0 +1,31 @@
+//
+//  HistoryVC.swift
+//  Buzzed
+//
+//  Created by Drew Lanning on 3/13/17.
+//  Copyright © 2017 Drew Lanning. All rights reserved.
+//
+
+import UIKit
+
+class HistoryVC: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
