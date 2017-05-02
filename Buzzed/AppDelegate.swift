362be02f19ae3b2d5a681599ff60e337c0bd3263
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2017-05-02 09:43:27 -0400

    updated status bar appearance

--- Buzzed/AppDelegate.swift
+++ Buzzed/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     let dm = DataManager()
     dm.decay()
+    
+    UIApplication.shared.statusBarStyle = .lightContent
     return true
   }
 
